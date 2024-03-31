256758d60a32ef4fe84a21469deb6b760f704283
Gerard Russell <gr@jrod.sh>
2024-03-31 19:51:33 -0400

    update nix bootstrap to support nushell

--- .config/nix_bootstrap.sh
+++ .config/nix_bootstrap.sh
@@ -3,3 +3,7 @@
 if command -v apt &> /dev/null; then # apt
     sudo xargs apt -y install <nix_deps.txt
 fi
+
+rm -rf "$HOME/Library/Application Support/nushell"
+mkdir -p "$HOME/Library/Application Support/nushell"
+ln -s "$HOME/Library/Application Support/nushell" $HOME/.config/nushell
