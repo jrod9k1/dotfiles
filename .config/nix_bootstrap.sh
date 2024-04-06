69a7ec7591f9ed34ea210bcbd774e6da348b869b
Gerard Russell <gr@jrod.sh>
2024-04-05 20:14:58 -0400

    add steps to bootstrap for nushell

--- .config/nix_bootstrap.sh
+++ .config/nix_bootstrap.sh
@@ -6,4 +6,5 @@ fi
 
 rm -rf "$HOME/Library/Application Support/nushell"
 mkdir -p "$HOME/Library/Application Support/nushell"
-ln -s "$HOME/Library/Application Support/nushell" $HOME/.config/nushell
+ln -s $HOME/.config/nushell/config.nu "$HOME/Library/Application Support/nushell/config.nu"
+ln -s $HOME/.config/nushell/env.nu "$HOME/Library/Application Support/nushell/env.nu"
